2ca629073098fccd5685796af902f27661962501
MansourAbiRizk <mansour.abirizk@syncwise.me>
2022-07-01 16:23:11 +0300

    PortUpdate

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ api.add_resource(co.CancelOrderByID, "/CancelOrderByID")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=5000)
